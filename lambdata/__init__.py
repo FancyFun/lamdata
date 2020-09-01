c5cca4ef3cf1c6b52e9a21034ac4ac5694273288
FancyFun <jordanhensiek3d@gmail.com>
2020-08-31 19:23:54 -0500

    new file:   Pipfile 	new file:   Pipfile.lock 	modified:   lambdata/__init__.py 	modified:   lambdata/example_module.py

---
+++ lambdata/__init__.py
@@ -0,0 +1,10 @@
+"""lambdata - a collection of Data Science helper functions
+"""
+
+#something will happen here I swear
+import numpy as np
+import pandas as pd
+
+def BaselineClassification(ytrain):
+  b = a.value_counts(normalize=True).max()
+  return b
---
+++ lambdata/example_module.py
@@ -0,0 +1,4 @@
+'''something else
+'''
+
+Favorit_restraunt = ['McDonolds', 'Subway', 'Jack n the Box', 'Red Lobster', 'Ponderosa','Apple Bees']
